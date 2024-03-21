5f285573cc29325ce8c87cba01e7ec5a39da6b7a
Choonho Son <choonho.son@snu.ac.kr>
2024-03-21 19:09:39 +0900

    fix output

--- dist/index.js
+++ dist/index.js
@@ -29054,7 +29054,7 @@ async function run() {
         issue_number: context.issue.number,
         labels: ['fail/signedoff']
       })
-      core.setOutput('fail', review)
+      core.setOutput('review', 'fail')
       return false
     } else {
       core.info('All commits have a valid signed-off-by')
@@ -29063,7 +29063,7 @@ async function run() {
         issue_number: context.issue.number,
         labels: ['pass/signedoff']
       })
-      core.setOutput('pass', review)
+      core.setOutput('review', 'pass')
       return true
     }
   } catch (error) {
